7ddb416038f2b8bc595399252bb54c84eded3e02
Joshua Mikel Jimenez <joshua.mikel.jimenez.neust.sic.cict@gmail.com>
2025-08-10 20:25:11 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', redirectTo: '/', pathMatch: 'full' },
+    {
+        path: '',
+        loadComponent: () => import('./views/home/home.component').then(m => m.HomeComponent),
+        data: {
+            title: 'Calton Datx',
+            description:'Description Meta Tag Content',
+            ogUrl: 'your og url'
+        }
+    },
+];
---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavigationBarComponent } from '../../components/navigation-bar/navigation-bar.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [NavigationBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
