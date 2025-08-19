a34ea58e70fb188d8c2163dd87e7ab031a414933
Joshua Mikel Jimenez <none>
2025-08-19 16:17:19 +0800

    fixed dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    provideAnimations()
+    provideRouter(routes)
   ]
 };
